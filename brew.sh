5b3282da818269772f42bad83a0448b893531841
Gasim Gasimzada <gasim@gasimzada.net>
2017-01-26 02:08:26 +0400

    Added most homebrew packages

---
+++ brew.sh
@@ -0,0 +1,31 @@
+# Homebrew installations
+
+# Applications
+brew cask install google-chrome
+brew cask install firefox
+
+brew cask install atom
+brew cask install robomongo
+brew cask install cocoarestclient
+brew cask install java
+brew cask install sequel-pro
+brew cask install docker-toolbox
+
+brew cask install handbrake
+brew cask install vlc
+
+brew cask install appcleaner
+brew cask install keepingyouawake
+brew cask install transmission
+brew cask install whatsapp
+brew cask install the-unarchiver
+brew cask install slack
+
+# Downloaders
+brew install wget
+brew install youtube-dl
+
+# Languages
+brew install python3
+brew install go
+

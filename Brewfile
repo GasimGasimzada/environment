409cc3c089c96745a09f36220cdf251635aae611
Gasim Gasimzada <gasim@appristas.io>
2017-05-19 01:57:59 +0400

    Added Brewfile + added apps
    - Moved all app installs to brew file
    - Changed brew.sh to install homebrew, brew bundle, and run brew bundle
    - Added Office365, AirServer, Transmit, Bartender, 1Password
    - Added MAS apps to be installed throug mas

---
+++ Brewfile
@@ -0,0 +1,70 @@
+# caskroom appdir
+cask_args appdir: '/Applications'
+
+# caskroom
+tap "caskroom/cask"
+
+# mas
+brew "mas"
+
+# browsers
+cask "firefox"
+cask "google-chrome"
+cask "vivaldi"
+
+# video tools
+cask "vlc"
+cask "handbrake"
+
+# chat apps
+cask "whatsapp"
+cask "slack"
+
+# utilities
+cask "alfred"
+cask "bartender"
+cask "keepingyouawake"
+cask "the-unarchiver"
+cask "transmission"
+cask "1password"
+cask "airserver"
+cask "appcleaner"
+
+# cli utilities
+brew "wget"
+brew "youtube-dl"
+brew "pwgen"
+
+# office365
+cask "microsoft-office"
+
+# languages
+brew "python"
+brew "python3"
+cask "java"
+
+# database utilities
+cask "sequel-pro"
+cask "robomongo"
+
+# rest clients
+cask "cocoarestclient"
+cask "postman"
+
+# text editors
+cask "sublime-text"
+
+# other dev tools
+cask "docker-toolbox"
+cask "transmit"
+brew "carthage"
+
+# mas apps
+mas "GarageBand", id: 682658836
+mas "iMovie", id: 408981434
+mas "Keynote", id: 409183694
+mas "Microsoft Remote Desktop", id: 715768417
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Parcel", id: 639968404
+mas "Xcode", id: 497799835
--- brew.sh
+++ brew.sh
@@ -1,52 +1,8 @@
-# Homebrew installations
-
-# Install homebrew
+# install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install caskroom
-brew tap caskroom/cask
-brew tap caskroom/fonts
+# install homebrew bundle
+brew tap homebrew/bundle
 
-# Install MAS
-brew install mas
-
-# Applications
-brew cask install alfred
-brew cask install google-chrome
-brew cask install firefox
-brew cask install vivaldi
-
-brew cask install sublime-text
-brew cask install robomongo
-brew cask install cocoarestclient
-brew cask install java
-brew cask install sequel-pro
-brew cask install docker-toolbox
-brew cask install postman
-
-brew cask install sketch
-
-brew cask install handbrake
-brew cask install vlc
-
-brew cask install appcleaner
-brew cask install keepingyouawake
-brew cask install transmission
-brew cask install whatsapp
-brew cask install the-unarchiver
-brew cask install slack
-
-# Downloaders
-brew install wget
-brew install youtube-dl
-
-# Languages
-brew install python
-brew install python3
-brew install go
-
-# Build tools
-brew install carthage
-
-# Other
-brew install pwgen
+# install apps from brewfile
+brew bundle

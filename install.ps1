9da1ee831ebcf3b05d09243719cb302ff0a1072d
gasim <me@gasim.xyz>
2021-10-24 21:35:05 +0200

    Use PS commands instead of Bash for PS install

--- install.ps1
+++ install.ps1
@@ -1,14 +1,13 @@
 # Create environment
 mkdir -p ~/.gasim-env
-cp .env ~/.gasim-env
-cp gasim.ps1 ~/.gasim-env
+Copy-Item gasim.ps1 ~/.gasim-env
 
 # Copy Oh my posh theme
-cp gasim.omp.json ~/.gasim-env
+Copy-Item gasim.omp.json ~/.gasim-env
 
 # Setup powershell profile
 New-Item -Path $PROFILE -ItemType File -Force
-echo ". ~/.gasim-env/gasim.ps1" > $PROFILE
+Write-Output ". ~/.gasim-env/gasim.ps1" > $PROFILE
 
 # Install plugins
 Install-Module Terminal-Icons -Scope CurrentUser

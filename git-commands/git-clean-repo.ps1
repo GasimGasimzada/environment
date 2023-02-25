35dc61b4672f2e54b3478da50a71849cca8d5cc5
gasim <me@gasim.xyz>
2023-02-25 21:07:31 +0100

    Remove remote branches when using git-clean-repo in powershell

--- git-commands/git-clean-repo.ps1
+++ git-commands/git-clean-repo.ps1
@@ -1,1 +1,1 @@
-git branch | Select-String -Pattern '^[^\*].*' | ForEach-Object { git branch -D $_.toString().Trim() }
+git pull --prune && git branch | Select-String -Pattern '^[^\*].*' | ForEach-Object { git branch -D $_.toString().Trim() }

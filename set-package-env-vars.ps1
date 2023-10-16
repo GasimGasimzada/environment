5717e6c0cea90d00639e33f9460b8f21fd44b8c0
gasim <me@gasim.xyz>
2023-10-17 00:01:33 +0200

    Add script to set package env vars for dev drive

---
+++ set-package-env-vars.ps1
@@ -0,0 +1,7 @@
+# Set up cache env vars
+setx /M npm_config_cache D:\.cache\npm
+setx /M VCPKG_DEFAULT_BINARY_CACHE D:\.cache\vcpkg
+setx /M PIP_CACHE_DIR D:\.cache\pip
+setx /M CARGO_HOME D:\.cache\cargo
+setx /M YARN_CACHE_FOLDER D:\.cache\yarn
+setx /M NUGET_PACKAGES D:\.cache\.nuget

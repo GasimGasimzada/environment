2fb49a80d442017309b50fc6e20e7011d9bcf0c4
Gasim Gasimzada <gasim@gasimzada.net>
2024-11-04 01:51:03 +0100

    Add more packages for Windows

--- install/setup-windows.ps1
+++ install/setup-windows.ps1
@@ -1,12 +1,19 @@
 # Install packages
 winget install JanDeDobbeleer.OhMyPosh -s winget
-winget install Microsoft.VisualStudioCode
 winget install Neovim.Neovim
 winget install BurntSushi.ripgrep.MSVC
 winget install sharkdp.fd
 winget install Schniz.fnm
-winget install ajeetdsouza.zoxide
-winget install --id Microsoft.WindowsTerminal -e
-winget install wez.wezterm
+winget install ajeetdsouza.zoxide 
+winget install JesseDuffield.lazygit
+winget install Microsoft.WindowsTerminal
+winget install VideoLAN.VLC
+winget install 7zip.7zip
+winget install VSCodium.VSCodium
+winget install Microsoft.PowerToys -s winget
+winget install NickeManarin.ScreenToGif
+winget install PDFsam.PDFsam
+winget install Git.Git
+winget install Mozilla.Thunderbird
 
 . install.ps1

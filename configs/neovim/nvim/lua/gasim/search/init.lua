30153ee5a962620465d1c2f467ae806f53fac161
Gasim Gasimzada <gasim@gasimzada.net>
2024-12-25 01:50:45 +0100

    Add binding for grug-far astgrep

--- configs/neovim/nvim/lua/gasim/search/init.lua
+++ configs/neovim/nvim/lua/gasim/search/init.lua
@@ -3,5 +3,9 @@ require("grug-far").setup()
 local grugFar = require("grug-far")
 
 vim.keymap.set("n", "<leader>fs", function()
-  grugFar.open()
-end, {})
+  grugFar.open({ engine = "ripgrep" })
+end)
+
+vim.keymap.set("n", "<leader>fa", function()
+  grugFar.open({ engine = "astgrep" })
+end)

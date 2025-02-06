3e29384bf8b86af0439bdb19d8d485df6c21f6ee
Gasim Gasimzada <gasim@gasimzada.net>
2025-02-06 18:24:39 +0100

    Enable treesitter highlight properly

--- configs/neovim/nvim/lua/gasim/plugins/lsp.lua
+++ configs/neovim/nvim/lua/gasim/plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "tsx", "typescript", "rust" },
         highlight = {
-          enabled = true,
+          enable = true,
           additional_vim_regex_highlighting = false,
         },
       })

117f23e17f776f77433e9ac67614fd6b549be2e0
Gasim Gasimzada <gasim@gasimzada.net>
2025-02-12 22:42:52 +0100

    Add LSP file rename to Neotree
    - Auto save renamed files

--- configs/neovim/nvim/lua/gasim/plugins/neotree.lua
+++ configs/neovim/nvim/lua/gasim/plugins/neotree.lua
@@ -9,6 +9,18 @@ return {
     },
     event = "VimEnter",
     config = function()
+      local function on_move(data)
+        Snacks.rename.on_rename_file(data.source, data.destination)
+
+        local buffers = vim.api.nvim_list_bufs()
+        for _, buf in ipairs(buffers) do
+          if vim.api.nvim_buf_get_option(buf, "modified") then
+            vim.api.nvim_buf_call(buf, function()
+              vim.cmd("write") -- Equivalent to :w
+            end)
+          end
+        end
+      end
       require("neo-tree").setup({
         filesystem = {
           follow_current_file = {
@@ -25,6 +37,17 @@ return {
           mappings = {
             ["<C-v>"] = "open_vsplit",
             ["<C-t>"] = "open_tabnew",
+          },
+        },
+
+        event_handlers = {
+          {
+            event = "file_moved",
+            handler = on_move,
+          },
+          {
+            event = "file_renamed",
+            handler = on_move,
           },
         },
       })

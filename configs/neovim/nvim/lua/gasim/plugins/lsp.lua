9b90959ff8abb4c641b7003c1da9031bd79b6fde
Gasim Gasimzada <gasim@gasimzada.net>
2025-03-25 20:04:57 +0100

    Replace cmp with blink.cmp
    - Add dap and dap ui configs

--- configs/neovim/nvim/lua/gasim/plugins/lsp.lua
+++ configs/neovim/nvim/lua/gasim/plugins/lsp.lua
@@ -92,36 +92,21 @@ return {
   },
 
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-vsnip",
-      "hrsh7th/vim-vsnip",
+    "saghen/blink.cmp",
+    version = "1.*",
+    -- @module "blink.cmp"
+    -- @type blink.cmp.Config
+    opts = {
+      keymap = { preset = "enter" },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = { documentation = { auto_show = true } },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    config = function()
-      local cmp = require("cmp")
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "vsnip" },
-        }, { name = "buffer" }),
-      })
-    end,
   },
 
   {
--- configs/neovim/nvim/lua/gasim/plugins/theme.lua
+++ configs/neovim/nvim/lua/gasim/plugins/theme.lua
@@ -8,6 +8,10 @@ return {
       require("catppuccin").setup({
         flavour = "mocha",
         show_end_of_buffer = true,
+        integrations = {
+          blink_cmp = true,
+          grug_far = true,
+        },
       })
 
       vim.cmd.colorscheme("catppuccin")

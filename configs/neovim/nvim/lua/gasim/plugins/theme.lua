79c68ddc3832580007499d64bf90d1cfe63d1b56
Gasim Gasimzada <gasim@gasimzada.net>
2026-01-13 22:00:21 +0100

    Remove catppuccin

--- configs/neovim/nvim/lua/gasim/plugins/theme.lua
+++ configs/neovim/nvim/lua/gasim/plugins/theme.lua
@@ -1,22 +1,8 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "GasimGasimzada/intent.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        show_end_of_buffer = true,
-        integrations = {
-          blink_cmp = true,
-          grug_far = true,
-        },
-      })
-    end,
-  },
-  {
-    "GasimGasimzada/intent.nvim",
     config = function()
       vim.cmd.colorscheme("intent")
     end,
